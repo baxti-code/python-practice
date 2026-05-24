0e87d4d1fe78c58b4589ce9f7f6c57404ce233c8
baxtiyor <baxti@Baxtiyors-MacBook-Pro.local>
2026-05-24 11:04:45 +0500

    Learnt Lists and list comprehension

---
+++ lists.py
@@ -0,0 +1,34 @@
+# nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# print(nested_list[0][1])  
+
+# my_list = ["apple", "banana", "cherry"]
+# my_list.pop()
+# print(my_list)
+
+# my_colors = ["red", "green", "blue", "yellow"]
+
+# print(my_colors[1])
+# print(my_colors[-1])
+
+# primary_colors = my_colors[:3]
+# print(primary_colors)
+
+# my_colors[3] = "orange"
+# print(my_colors)
+
+# my_colors.append("purple")
+# print(my_colors)
+
+# my_colors.insert(1, "teal")
+# print(my_colors)
+
+# my_colors.remove("green")
+# print(my_colors)
+
+# my_colors.pop(3)
+# print(my_colors)
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+greater_numbers = [x for x in numbers if x > 5]
+print(greater_numbers)
