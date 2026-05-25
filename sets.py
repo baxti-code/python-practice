96b2769448bd240bffd0c75ff1691c496face5b6
baxtiyor <baxtixolmominov1@gmail.com>
2026-05-25 21:03:02 +0500

    First commit

---
+++ sets.py
@@ -0,0 +1,11 @@
+# my_sets = {1, 2, 3, 4, 5}
+# print(type(my_sets))
+# string_set = {"This", "is", "for", "test"}
+# print(type(string_set))
+
+# empty_set = set()
+# print(empty_set)
+
+my_set = {1, 2, 3, 4}
+element = my_set.pop()
+print(element)
