6bcfc90cb15341f577ad30c19522f22ce4f4783d
baxtiyor <baxtixolmominov1@gmail.com>
2026-06-13 14:50:33 +0500

    update Class_inheritence

--- Classes/class_inheritence.py
+++ Classes/class_inheritence.py
@@ -1,61 +1,113 @@
-class Shaxs:
-    """Shaxslar haqida ma'lumot"""
-    def __init__(self,ism,familiya,passport,tyil):
-        self.ism = ism
-        self.familiya = familiya
-        self.passport = passport
-        self.tyil = tyil
+# class Shaxs:
+#     """Shaxslar haqida ma'lumot"""
+#     def __init__(self,ism,familiya,passport,tyil):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.passport = passport
+#         self.tyil = tyil
     
-    def get_info(self):
-        """Shaxs haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya}. "
-        info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
-        return info
+#     def get_info(self):
+#         """Shaxs haqida ma'lumot"""
+#         info = f"{self.ism} {self.familiya}. "
+#         info += f"Passport:{self.passport}, {self.tyil}-yilda tug`ilgan"
+#         return info
         
-    def get_age(self,yil):
-        """Shaxsning yoshini qaytaruvchi metod"""
-        return yil - self.tyil
+#     def get_age(self,yil):
+#         """Shaxsning yoshini qaytaruvchi metod"""
+#         return yil - self.tyil
 
-class Talaba(Shaxs):
-    """Talaba klassi"""
-    def __init__(self,ism,familiya,passport,tyil,idraqam,manzil):
-        """Talabaning xususiyatlari"""
-        super().__init__(ism, familiya, passport, tyil)
-        self.idraqam = idraqam
-        self.bosqich = 1
-        self.manzil = manzil
+# class Talaba(Shaxs):
+#     """Talaba klassi"""
+#     def __init__(self,ism,familiya,passport,tyil,idraqam,manzil):
+#         """Talabaning xususiyatlari"""
+#         super().__init__(ism, familiya, passport, tyil)
+#         self.idraqam = idraqam
+#         self.bosqich = 1
+#         self.manzil = manzil
     
-    def get_id(self):
-        """Talabaning ID raqami"""
-        return self.idraqam
+#     def get_id(self):
+#         """Talabaning ID raqami"""
+#         return self.idraqam
     
-    def get_bosqich(self):
-        """Talabaning o'qish bosqichi"""
-        return self.bosqich
+#     def get_bosqich(self):
+#         """Talabaning o'qish bosqichi"""
+#         return self.bosqich
     
-    def get_info(self):
-        """Talaba haqida ma'lumot"""
-        info = f"{self.ism} {self.familiya}. "
-        info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
-        return info
+#     def get_info(self):
+#         """Talaba haqida ma'lumot"""
+#         info = f"{self.ism} {self.familiya}. "
+#         info += f"{self.get_bosqich()}-bosqich. ID raqami: {self.idraqam}"
+#         return info
     
 
-class Manzil:
-    """Manzil saqlash uchun klass"""
-    def __init__(self,uy,kocha,tuman,viloyat):
-        """Manzil xususiyatlari"""
-        self.uy = uy
-        self.kocha = kocha
-        self.tuman = tuman
-        self.viloyat = viloyat
+# class Manzil:
+#     """Manzil saqlash uchun klass"""
+#     def __init__(self,uy,kocha,tuman,viloyat):
+#         """Manzil xususiyatlari"""
+#         self.uy = uy
+#         self.kocha = kocha
+#         self.tuman = tuman
+#         self.viloyat = viloyat
     
-    def get_manzil(self):
-        """Manzilni ko'rish"""
-        manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
-        manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
-        return manzil
+#     def get_manzil(self):
+#         """Manzilni ko'rish"""
+#         manzil = f"{self.viloyat} viloyati, {self.tuman} tumani, "
+#         manzil += f"{self.kocha} ko'chasi, {self.uy}-uy"
+#         return manzil
 
 
-talaba_manzil = Manzil(12,'Olmazor',"Bog'bon","Samarqand")
-talaba = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",talaba_manzil)
-print(talaba.manzil.get_manzil())
+# talaba_manzil = Manzil(12,'Olmazor',"Bog'bon","Samarqand")
+# talaba = Talaba("Valijon","Aliyev","FA112299",2000,"0000012",talaba_manzil)
+# print(talaba.manzil.get_manzil())
+
+# #Accessing the inner class
+# class Outer:
+#   def __init__(self):
+#     self.name = "Outer"
+
+#   class Inner:
+#     def __init__(self):
+#       self.name = "Inner"
+
+#     def display(self):
+#       print("Hello from inner class")
+
+# # outer = Outer()
+# # inner = outer.Inner()
+# # inner.display()
+# # OR
+# Outer().Inner().display()
+
+
+# class Outer:
+#   def __init__(self):
+#     self.name = "Emil"
+
+#   class Inner:
+#     def __init__(self, outer):
+#       self.outer = outer
+
+#     def display(self):
+#       print(f"Outer class name: {self.outer.name}")
+
+# outer = Outer()
+# inner = outer.Inner(outer)
+# inner.display()
+
+#Creating multiple inner classes
+class Computer:
+  def __init__(self):
+    self.cpu = self.CPU()
+    self.ram = self.RAM()
+
+  class CPU:
+    def process(self):
+      print("Processing data...")
+
+  class RAM:
+    def store(self):
+      print("Storing data...")
+
+computer = Computer()
+computer.cpu.process()
+computer.ram.store()
